52bf36fbb666fd3927abf3e922c5cebf31c4c385
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-02-15 13:18:57 +0100

    Solucionado mensajes error validación

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl, ValidationErrors } from '@angular/forms';
 import { BudgetService } from '../../services/budget.service';
 import { PanelComponent } from '../panel/panel.component';
-import { NgIf } from '@angular/common';
+import { JsonPipe, NgIf } from '@angular/common';
 import { serveis, panelServeis, client } from '../../interfaces/formularis.interface';
 import { ListPptosComponent } from '../list-pptos/list-pptos.component';
 import { PersonalValidacions } from '../validacions/personal-validacions';
@@ -14,7 +14,8 @@ import { PersonalValidacions } from '../validacions/personal-validacions';
     ReactiveFormsModule,
     PanelComponent,
     ListPptosComponent,
-    NgIf
+    NgIf,
+    JsonPipe
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
@@ -24,10 +25,10 @@ export class HomeComponent {
 
   public serveis = this.BudgetService.getServeis();
   pptoForm: FormGroup;
-  nomErrors: string = "";
-  telErrors: string = "";
-  emailErrors: string = "";
-  totPptoErrors: string = "";
+  nomErrors: boolean = false;
+  telErrors: boolean = false;
+  emailErrors: boolean = false;
+  totPptoErrors: boolean = false;
 
 
   constructor(
@@ -46,6 +47,10 @@ export class HomeComponent {
       tel: ['', [Validators.required, PersonalValidacions.telefonoValido]],
       email: ['', [Validators.required, PersonalValidacions.emailValido]],
     });
+
+
+
+
 
 
   }
@@ -82,62 +87,17 @@ export class HomeComponent {
   }
 
   demanarPpto(event: Event) {
-    event.preventDefault(); // Evita que el formulari s'enviï
-
-    if (this.pptoForm.valid && this.pptoForm.touched) {
-      const nouPressupost = this.pptoForm.getRawValue();
+    const nouPressupost = this.pptoForm.getRawValue();
       console.log("Ptpo capturado: ", nouPressupost);
       this.BudgetService.guardarPpto(nouPressupost);
       this.pptoForm.reset();
       this.pptoForm.get('totPpto')?.reset(0);
       this.pptoForm.get('numPagines')?.reset(1);
       this.pptoForm.get('numIdiomes')?.reset(1);
-    } else {
-      // Mostrar un mensaje al usuario indicando que hay errores en el formulario
-      console.log("Formulario no válido");
-    }
   }
-  
+
+
 }
-
-
-
-/* // Validaciones
-
-nombreValido(control: FormControl): ValidationErrors | null {
-  const regex = new RegExp(/^[a-zA-Záéíóúñ ]+$/);
-  if (!regex.test(control.value)) {
-    return { nombreInvalido: true };
-  }
-  return null;
-}
-
-  telefonoValido(control: FormControl): ValidationErrors | null {
-  const regexTel = /^[0-9]{9}$/;
-  if (!regexTel.test(control.value)) {
-    return { telefonoInvalido: true };
-  }
-  return null;
-}
-
-  emailValido(control: FormControl): ValidationErrors | null {
-  const regexMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (!regexMail.test(control.value)) {
-    return { emailInvalido: true };
-  }
-  return null;
-}
-
-totalValido(control: FormControl): ValidationErrors | null {
-
-  if ((control.value) === 0) {
-    return { totalInvalido: true };
-  }
-  return null;
-}
-
-*/
-
 
 
 
